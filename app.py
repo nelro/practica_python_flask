c40396f4ce69bbb0d4f7d9d1578a0b7773430f50
nelson rodrigo mamani cuyuña <nelro.sint@gmail.com>
2024-10-09 22:41:39 -0400

    2do_com_flask

--- app.py
+++ app.py
@@ -3,9 +3,25 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-
-def index():
+def inicio():
     return render_template("inicio.html")
+
+#para poder dirigirme a la pagina quienes somos 
+@app.route("/quienes_somos")
+def quienes_somos():
+    return render_template("quienes_somos.html")
+
+@app.route("/servicios")
+def servicios():
+    return render_template("servicios.html")
+
+@app.route("/noticias")
+def noticias():
+    return render_template("noticias.html")
+
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
